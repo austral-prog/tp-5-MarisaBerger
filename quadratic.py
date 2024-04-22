aac5799b45c67d012483965d257d8b4a27a8b36b
MarisaBerger <163553459+MarisaBerger@users.noreply.github.com>
2024-04-22 19:03:38 -0300

    Update quadratic.py

--- quadratic.py
+++ quadratic.py
@@ -16,21 +16,23 @@ def value_y(a, b, c, x):
     return y
 
 def to_string(a, b, c):
-    if (a == 0 and b == 0):
-        return f"f(x) = {c}"
-    elif a == 0 :
-        return f"f(x) = {b} * X + {c}"
-    elif (b == 0):
-        return f"f(x) = {a} * X^2 + {c}"
-    else:
+    if a!=0 and b!=0 and c!=0:
         return f"f(x) = {a} * X^2 + {b} * X + {c}"
-     
+    elif a==0 and b!=0 and c!=0:
+        return f"f(x) = {b} * X + {c}"
+    elif a!=0 and b==0 and c!=0:
+        return f"f(x) = {a} * X^2 + {c}"
+    elif a!=0 and b!=0 and c==0:
+        return f"f(x) = {a} * X^2 + {b} * X"
+    elif a==0 and b==0 and c!=0:
+        return f"f(x) = {c}" 
+
 def derivation(a, b, c):
-    if (a == 0 and b ==0):
+    if (a == 0 and b == 0):
+        return f"f'(x) = 0"
+    elif (a == 0 and b!= 0):
         return f"f'(x) = {b}"
-    elif a == 0:
-        return f"f'(x) = {b}"
-    elif b == 0:
+    elif (a != 0 and b == 0):
         return f"f'(x) = {2*a} * X"
-    else:
+    elif (a != 0 and b != 0):
         return f"f'(x) = {2*a} * X + {b}"
